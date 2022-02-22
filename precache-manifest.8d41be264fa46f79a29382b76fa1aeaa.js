e4f5221e55c72632cb6e0d00c05b2f3d5a5dd89c
bagues33 <bagustofan@yahoo.com>
2022-02-22 16:33:43 +0700

    Updates

---
+++ precache-manifest.8d41be264fa46f79a29382b76fa1aeaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e7980c950f265773d0d",
+    "url": "/react-todolist/static/css/main.ebe822b8.chunk.css"
+  },
+  {
+    "revision": "8e7980c950f265773d0d",
+    "url": "/react-todolist/static/js/main.8e7980c9.chunk.js"
+  },
+  {
+    "revision": "c52e6f36d7d880e59896",
+    "url": "/react-todolist/static/js/1.c52e6f36.chunk.js"
+  },
+  {
+    "revision": "15e18c78eb78a1dce817",
+    "url": "/react-todolist/static/js/runtime~main.15e18c78.js"
+  },
+  {
+    "revision": "0cc99545ee9502d142df2a45d9e31db8",
+    "url": "/react-todolist/index.html"
+  }
+];
